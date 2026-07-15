91a93fa4cb0c682c81b3dd182a078049af9bd9f8
Mark Saushkin <mark.saushkin@outlook.com>
2026-07-15 23:03:06 +0400

    feat(fundamentals): add RAII FileHandle wrapper

---
+++ labs/fundamentals/RaiiWrapperOverResource/FileHandle.cpp
@@ -0,0 +1,62 @@
+﻿//
+// Created by marks on 15.07.2026.
+//
+
+#include <utility>
+#include "FileHandle.h"
+
+
+FileHandle::FileHandle(
+            const std::string& path,
+            const char* mode
+        )
+    : file_(std::fopen(path.c_str(), mode))
+{
+    if (file_ == nullptr)
+        throw std::runtime_error("Failed to open file: " + path);
+}
+
+
+FileHandle::~FileHandle() {
+    Close();
+}
+
+
+// Move constructor
+// A move does not copy ownership. It transfers ownership to a new object.
+FileHandle::FileHandle(FileHandle&& other) noexcept
+        : file_(std::exchange(other.file_, nullptr)) // std::exchange performs two operations
+                                                                 // file_ = other.file_;
+                                                                 // other.file_ = nullptr;
+{
+}
+
+
+// Move assignment operator
+FileHandle & FileHandle::operator=(FileHandle &&other) noexcept {
+    if (this == &other)
+        return *this;
+
+    Close();
+
+    file_ = std::exchange(other.file_, nullptr);
+
+    return *this;
+}
+
+
+bool FileHandle::IsOpen() const noexcept {
+    return file_ != nullptr;
+}
+
+std::FILE * FileHandle::Get() const noexcept {
+    return file_;
+}
+
+void FileHandle::Close() noexcept {
+    if (file_ == nullptr)
+        return;
+
+    std::fclose(file_);
+    file_ = nullptr;
+}
---
+++ labs/fundamentals/RaiiWrapperOverResource/FileHandle.h
@@ -0,0 +1,36 @@
+﻿//
+// Created by marks on 15.07.2026.
+//
+
+#ifndef PRODUCTIONCPPROADMAP_FILEHANDLE_H
+#define PRODUCTIONCPPROADMAP_FILEHANDLE_H
+
+#include <filesystem>
+
+
+class FileHandle final {
+public:
+    explicit FileHandle(
+        const std::string& path,
+        const char* mode
+    );
+
+    ~FileHandle();
+
+    FileHandle(const FileHandle&) = delete;
+    FileHandle(FileHandle&& other) noexcept;
+
+    FileHandle& operator=(const FileHandle&) = delete;
+    FileHandle& operator=(FileHandle&& other) noexcept;
+
+    [[nodiscard]] bool IsOpen() const noexcept;
+    [[nodiscard]] std::FILE* Get() const noexcept;
+
+private:
+    void Close() noexcept;
+
+    std::FILE* file_{nullptr};
+};
+
+
+#endif //PRODUCTIONCPPROADMAP_FILEHANDLE_H
