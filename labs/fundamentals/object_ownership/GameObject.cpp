1c94a90e77045309f32292a048199a1a72533e14
Mark Saushkin <mark.saushkin@outlook.com>
2026-07-20 20:40:56 +0400

    feat(fundamentals): add GameObject with object ownership mechanics

---
+++ labs/fundamentals/object_ownership/GameObject.cpp
@@ -0,0 +1,39 @@
+﻿//
+// Created by marks on 20.07.2026.
+//
+
+#include <iostream>
+#include "GameObject.h"
+
+namespace production_cpp::object_ownership {
+    GameObject::GameObject(std::string name)
+        : name_(std::move(name))
+    { }
+
+
+    GameObject &GameObject::AddChild(std::unique_ptr<GameObject> child) {
+        if (child == nullptr)
+            throw std::invalid_argument("child is nullptr");
+
+        auto c = child.get();
+        child->parent_ = this;
+        children_.push_back(std::move(child));
+
+        return *c;
+    }
+
+
+    const std::string &GameObject::Name() const noexcept { return name_; }
+
+
+    GameObject *GameObject::Parent() noexcept { return parent_; }
+
+    const GameObject *GameObject::Parent() const noexcept { return parent_; }
+
+
+    std::size_t GameObject::ChildCount() const noexcept { return children_.size(); }
+
+    GameObject &GameObject::ChildAt(std::size_t index) { return *children_[index]; }
+
+    const GameObject &GameObject::ChildAt(std::size_t index) const { return *children_[index]; }
+}
---
+++ labs/fundamentals/object_ownership/GameObject.h
@@ -0,0 +1,53 @@
+﻿//
+// Created by marks on 20.07.2026.
+//
+
+#ifndef PRODUCTIONCPPROADMAP_GAMEOBJECT_H
+#define PRODUCTIONCPPROADMAP_GAMEOBJECT_H
+
+#include <cstddef>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace production_cpp::object_ownership {
+    class GameObject final {
+    public:
+        explicit GameObject(std::string name);
+
+        GameObject(const GameObject &) = delete;
+
+        GameObject &operator=(const GameObject &) = delete;
+
+        GameObject(GameObject &&) = delete;
+
+        GameObject &operator=(GameObject &&) = delete;
+
+        virtual ~GameObject() = default;
+
+        GameObject &AddChild(std::unique_ptr<GameObject> child);
+
+        [[nodiscard]] const std::string &Name() const noexcept;
+
+        [[nodiscard]] GameObject *Parent() noexcept;
+
+        [[nodiscard]] const GameObject *Parent() const noexcept;
+
+        [[nodiscard]] std::size_t ChildCount() const noexcept;
+
+        GameObject &ChildAt(std::size_t index);
+
+        [[nodiscard]] const GameObject &ChildAt(std::size_t index) const;
+
+    private:
+        std::string name_;
+
+        // Non-owning pointer.
+        GameObject *parent_{nullptr};
+
+        // Sole Ownership of Subsidiary Facilities.
+        std::vector<std::unique_ptr<GameObject>> children_;
+    };
+}
+
+#endif //PRODUCTIONCPPROADMAP_GAMEOBJECT_H
